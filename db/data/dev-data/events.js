81dff9ac43d142f540ff1623c5d798433ba69b5b
Jookz <j.chatterton456@gmail.com>
2023-11-09 15:20:07 +0000

    added event images

--- db/data/dev-data/events.js
+++ db/data/dev-data/events.js
@@ -1,138 +1,189 @@
 module.exports = [
-    {
-        event_id: 1,
-        creator_id: 1,
-        date: 1697043600000,
-        short_description: "Trip to the park",
-        description: "Calling all fellow hiking lovers - lets meet and have an amazing time hiking at Gran de la Sagrera",
-        location: "C/ Gran de la Sagrera, 08027 Barcelona, Spain"
-    },
-    {
-        event_id: 2,
-        creator_id: 1,
-        date: 1694451600000,
-        short_description: "Trip to the Picasso Museum",
-        description: "Calling all fellow art enthusiasts! Picasso Museum was on my to-see-list for a very long time now and the entry fee is only £15pp.",
-        location: "C/ de Montcada, 15-23, 08003 Barcelona, Spain"
-    },
-    {
-        event_id: 3,
-        creator_id: 2,
-        date: 1707670800000,
-        short_description: "Trip to the Diocletian's Palace",
-        description: "Diocletian's Palace is a huge, fortress-like palace served as a residence for a Roman Emperor & the military garrison.",
-        location: "C/ de Montcada, 15-23, 08003 Barcelona, Spain"
-    },
-    {
-        event_id: 4,
-        creator_id: 2,
-        date: 1707670800000,
-        short_description: "Trip to the museum of broken relationships",
-        description: "The Museum of Broken Relationships is a museum in Zagreb, Croatia, dedicated to failed love relationships.",
-        location: "Ćirilometodska ul. 2, 10000, Zagreb, Croatia"
-    },
-    {
-        event_id: 5,
-        creator_id: 3,
-        date: 1707670800000,
-        short_description: "Trip to the fortress of Klis",
-        description: "Klis Fortress is a medieval fortress situated above a village bearing the same name, near the city of Split, in central Dalmatia, Croatia.",
-        location: "Kliška tvrđava, 21231, Klis, Croatia"
-    },
-    {
-        event_id: 6,
-        creator_id: 3,
-        date: 1707670800000,
-        short_description: "Trip to the Krka National Park",
-        description: "Krka National Park is one of the Croatian national parks, named after the river Krka that it encloses.",
-        location: "Lozovac, Croatia"
-    },
-    {
-        event_id: 7,
-        creator_id: 4,
-        date: 1707670800000,
-        short_description: "Trip to the Plitvice Lakes National Park",
-        description: "Plitvice Lakes National Park is a 295-sq.-km forest reserve in central Croatia. It's known for a chain of 16 terraced lakes, joined by waterfalls, that extend into a limestone canyon.",
-        location: "Plitvička Jezera, Croatia"
-    },
-    {
-        event_id: 8,
-        creator_id: 4,
-        date: 1707670800000,
-        short_description: "Trip to the Dubrovnik Old Town",
-        description: "Dubrovnik is a city in southern Croatia fronting the Adriatic Sea. It's known for its distinctive Old Town, encircled with massive stone walls completed in the 16th century.",
-        location: "Dubrovnik, Croatia"
-    },
-    {
-        event_id: 9,
-        creator_id: 5,
-        date: 1707670800000,
-        short_description: "Trip to the Zagreb Cathedral",
-        description: "Zagreb Cathedral on Kaptol is a Roman Catholic institution and not only the tallest building in Croatia but also the most monumental sacral building in Gothic style southeast of the Alps.",
-        location: "Kaptol 31, 10000, Zagreb, Croatia"
-    },
-    {
-        event_id: 10,
-        creator_id: 5,
-        date: 1707670800000,
-        short_description: "Trip to the Mirogoj Cemetery",
-        description: "The Mirogoj Cemetery is a cemetery park that is considered to be among the more noteworthy landmarks in the City of Zagreb.",
-        location: "Aleja Hermanna Bollea 27, 10000, Zagreb, Croatia"
-    },
-    {
-        event_id: 11,
-        creator_id: 6,
-        date: 1707670800000,
-        short_description: "Trip to the Museum of Broken Relationships",
-        description: "The Museum of Broken Relationships is a museum in Zagreb, Croatia, dedicated to failed love relationships.",
-        location: "Ćirilometodska ul. 2, 10000, Zagreb, Croatia"
-    },
-    {
-        event_id: 12,
-        creator_id: 6,
-        date: 1707670800000,
-        short_description: "Trip to the Zagreb Cathedral",
-        description: "Zagreb Cathedral on Kaptol is a Roman Catholic institution and not only the tallest building in Croatia but also the most monumental sacral building in Gothic style southeast of the Alps.",
-        location: "Kaptol 31, 10000, Zagreb, Croatia"
-    },
-    {
-        event_id: 13,
-        creator_id: 7,
-        date: 1707670800000,
-        short_description: "Trip to the Diocletian's Palace",
-        description: "Diocletian's Palace is a huge, fortress-like palace served as a residence for a Roman Emperor & the military garrison.",
-        location: "C/ de Montcada, 15-23, 08003 Barcelona, Spain"
-    },
-    {
-        event_id: 14,
-        creator_id: 7,
-        date: 1707670800000,
-        short_description: "Trip to the museum of broken relationships",
-        description: "The Museum of Broken Relationships is a museum in Zagreb, Croatia, dedicated to failed love relationships.",
-        location: "Ćirilometodska ul. 2, 10000, Zagreb, Croatia"
-    },
-    {
-        event_id: 15,
-        creator_id: 8,
-        date: 1707670800000,
-        short_description: "Trip to the fortress of Klis",
-        description: "Klis Fortress is a medieval fortress situated above a village bearing the same name, near the city of Split, in central Dalmatia, Croatia.",
-        location: "Kliška tvrđava, 21231, Klis, Croatia"
-    },
-    {
-        event_id: 16,
-        creator_id: 8,
-        date: 1707670800000,
-        short_description: "Trip to the Krka National Park",
-        description: "Krka National Park is one of the Croatian national parks, named after the river Krka that it encloses.",
-        location: "Lozovac, Croatia"
-    },
-    {
-        event_id: 17,
-        creator_id: 9,
-        date: 1707670800000,
-        short_description: "Trip to the Plitvice Lakes National Park",
-        description: "Plitvice Lakes National Park is a 295-sq.-km forest reserve in central Croatia. It's known for a chain of 16 terraced lakes, joined by waterfalls, that extend into a limestone canyon.",
-        location: "Plitvička Jezera, Croatia"
-    }
+  {
+    event_id: 1,
+    creator_id: 1,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.idealista.com%2Fmaps%2Fbarcelona-barcelona%2Fcalle-gran-de-la-sagrera%2F147%2F&psig=AOvVaw2qvuBs9NUdUZUYPd1Ye82Y&ust=1699628765638000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPDVg9mYt4IDFQAAAAAdAAAAABAE",
+    date: 1697043600000,
+    short_description: "Trip to the park",
+    description:
+      "Calling all fellow hiking lovers - lets meet and have an amazing time hiking at Gran de la Sagrera",
+    location: "C/ Gran de la Sagrera, 08027 Barcelona, Spain",
+  },
+  {
+    event_id: 2,
+    creator_id: 1,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fmuseupicassobcn.cat%2Fen%2Fplan-your-visit%2Fbuy-tickets-and-opening-hours&psig=AOvVaw3wlXzKyQRtYTuVHxVWYoCi&ust=1699628857723000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOi8oYWZt4IDFQAAAAAdAAAAABAE",
+    date: 1694451600000,
+    short_description: "Trip to the Picasso Museum",
+    description:
+      "Calling all fellow art enthusiasts! Picasso Museum was on my to-see-list for a very long time now and the entry fee is only £15pp.",
+    location: "C/ de Montcada, 15-23, 08003 Barcelona, Spain",
+  },
+  {
+    event_id: 3,
+    creator_id: 2,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.britannica.com%2Ftopic%2FPalace-of-Diocletian&psig=AOvVaw1rXA8uJ9CEgMW1JgKT8r0F&ust=1699628906351000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIjCuJyZt4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the Diocletian's Palace",
+    description:
+      "Diocletian's Palace is a huge, fortress-like palace served as a residence for a Roman Emperor & the military garrison.",
+    location: "C/ de Montcada, 15-23, 08003 Barcelona, Spain",
+  },
+  {
+    event_id: 4,
+    creator_id: 2,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.npr.org%2Fsections%2Fparallels%2F2013%2F09%2F30%2F227738868%2Fa-history-of-love-gone-wrong-all-in-one-croatian-museum&psig=AOvVaw3Rdcx9nsRKisNwuIzgOrfF&ust=1699628953483000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKiHkbOZt4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the museum of broken relationships",
+    description:
+      "The Museum of Broken Relationships is a museum in Zagreb, Croatia, dedicated to failed love relationships.",
+    location: "Ćirilometodska ul. 2, 10000, Zagreb, Croatia",
+  },
+  {
+    event_id: 5,
+    creator_id: 3,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.lonelyplanet.com%2Fcroatia%2Fdalmatia%2Fsplit%2Fattractions%2Fklis-fortress%2Fa%2Fpoi-sig%2F1544570%2F358789&psig=AOvVaw0I0AZFdFXfTNaQHem9pPlA&ust=1699629000697000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDLnsmZt4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the fortress of Klis",
+    description:
+      "Klis Fortress is a medieval fortress situated above a village bearing the same name, near the city of Split, in central Dalmatia, Croatia.",
+    location: "Kliška tvrđava, 21231, Klis, Croatia",
+  },
+  {
+    event_id: 6,
+    creator_id: 3,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.sail-croatia.com%2Fset-sail%2Fthe-ultimate-guide-to-krka-national-park&psig=AOvVaw0q6MBaI8Aokw4KpDCdVLUW&ust=1699629049282000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJiauuCZt4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the Krka National Park",
+    description:
+      "Krka National Park is one of the Croatian national parks, named after the river Krka that it encloses.",
+    location: "Lozovac, Croatia",
+  },
+  {
+    event_id: 7,
+    creator_id: 4,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.akvillas.com%2Fdestinations%2Fcroatia%2Fexplore-the-stunning-plitvice-lakes&psig=AOvVaw1SZPC6hiCKYaTkyt36_7pH&ust=1699629100042000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMjHsfiZt4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the Plitvice Lakes National Park",
+    description:
+      "Plitvice Lakes National Park is a 295-sq.-km forest reserve in central Croatia. It's known for a chain of 16 terraced lakes, joined by waterfalls, that extend into a limestone canyon.",
+    location: "Plitvička Jezera, Croatia",
+  },
+  {
+    event_id: 8,
+    creator_id: 4,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.timeout.com%2Fcroatia%2Fthings-to-do%2Fthings-to-do-in-dubrovniks-old-town&psig=AOvVaw0KPLERx5KJbdZIyaq4sZx0&ust=1699629138751000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjhy4qat4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the Dubrovnik Old Town",
+    description:
+      "Dubrovnik is a city in southern Croatia fronting the Adriatic Sea. It's known for its distinctive Old Town, encircled with massive stone walls completed in the 16th century.",
+    location: "Dubrovnik, Croatia",
+  },
+  {
+    event_id: 9,
+    creator_id: 5,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.visitzagreb.hr%2Fzagreb%2Fzagreb-cathedral%2F&psig=AOvVaw0JDHYrJzTw-lertNA-gAeL&ust=1699629272294000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCn7Mqat4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the Zagreb Cathedral",
+    description:
+      "Zagreb Cathedral on Kaptol is a Roman Catholic institution and not only the tallest building in Croatia but also the most monumental sacral building in Gothic style southeast of the Alps.",
+    location: "Kaptol 31, 10000, Zagreb, Croatia",
+  },
+  {
+    event_id: 10,
+    creator_id: 5,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.visitzagreb.hr%2Fzagreb%2Fmirogoj-cemetery%2F&psig=AOvVaw1GpN7oyjQdZShZNoAUmF2h&ust=1699629309059000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMClp9yat4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the Mirogoj Cemetery",
+    description:
+      "The Mirogoj Cemetery is a cemetery park that is considered to be among the more noteworthy landmarks in the City of Zagreb.",
+    location: "Aleja Hermanna Bollea 27, 10000, Zagreb, Croatia",
+  },
+  {
+    event_id: 11,
+    creator_id: 6,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.npr.org%2Fsections%2Fparallels%2F2013%2F09%2F30%2F227738868%2Fa-history-of-love-gone-wrong-all-in-one-croatian-museum&psig=AOvVaw3Rdcx9nsRKisNwuIzgOrfF&ust=1699628953483000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKiHkbOZt4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the Museum of Broken Relationships",
+    description:
+      "The Museum of Broken Relationships is a museum in Zagreb, Croatia, dedicated to failed love relationships.",
+    location: "Ćirilometodska ul. 2, 10000, Zagreb, Croatia",
+  },
+  {
+    event_id: 12,
+    creator_id: 6,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.visitzagreb.hr%2Fzagreb%2Fzagreb-cathedral%2F&psig=AOvVaw0JDHYrJzTw-lertNA-gAeL&ust=1699629272294000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCn7Mqat4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the Zagreb Cathedral",
+    description:
+      "Zagreb Cathedral on Kaptol is a Roman Catholic institution and not only the tallest building in Croatia but also the most monumental sacral building in Gothic style southeast of the Alps.",
+    location: "Kaptol 31, 10000, Zagreb, Croatia",
+  },
+  {
+    event_id: 13,
+    creator_id: 7,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.britannica.com%2Ftopic%2FPalace-of-Diocletian&psig=AOvVaw1rXA8uJ9CEgMW1JgKT8r0F&ust=1699628906351000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIjCuJyZt4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the Diocletian's Palace",
+    description:
+      "Diocletian's Palace is a huge, fortress-like palace served as a residence for a Roman Emperor & the military garrison.",
+    location: "C/ de Montcada, 15-23, 08003 Barcelona, Spain",
+  },
+  {
+    event_id: 14,
+    creator_id: 7,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.npr.org%2Fsections%2Fparallels%2F2013%2F09%2F30%2F227738868%2Fa-history-of-love-gone-wrong-all-in-one-croatian-museum&psig=AOvVaw3Rdcx9nsRKisNwuIzgOrfF&ust=1699628953483000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKiHkbOZt4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the museum of broken relationships",
+    description:
+      "The Museum of Broken Relationships is a museum in Zagreb, Croatia, dedicated to failed love relationships.",
+    location: "Ćirilometodska ul. 2, 10000, Zagreb, Croatia",
+  },
+  {
+    event_id: 15,
+    creator_id: 8,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.lonelyplanet.com%2Fcroatia%2Fdalmatia%2Fsplit%2Fattractions%2Fklis-fortress%2Fa%2Fpoi-sig%2F1544570%2F358789&psig=AOvVaw0I0AZFdFXfTNaQHem9pPlA&ust=1699629000697000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDLnsmZt4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the fortress of Klis",
+    description:
+      "Klis Fortress is a medieval fortress situated above a village bearing the same name, near the city of Split, in central Dalmatia, Croatia.",
+    location: "Kliška tvrđava, 21231, Klis, Croatia",
+  },
+  {
+    event_id: 16,
+    creator_id: 8,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.sail-croatia.com%2Fset-sail%2Fthe-ultimate-guide-to-krka-national-park&psig=AOvVaw0q6MBaI8Aokw4KpDCdVLUW&ust=1699629049282000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJiauuCZt4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the Krka National Park",
+    description:
+      "Krka National Park is one of the Croatian national parks, named after the river Krka that it encloses.",
+    location: "Lozovac, Croatia",
+  },
+  {
+    event_id: 17,
+    creator_id: 9,
+    event_img:
+      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.akvillas.com%2Fdestinations%2Fcroatia%2Fexplore-the-stunning-plitvice-lakes&psig=AOvVaw1SZPC6hiCKYaTkyt36_7pH&ust=1699629100042000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMjHsfiZt4IDFQAAAAAdAAAAABAE",
+    date: 1707670800000,
+    short_description: "Trip to the Plitvice Lakes National Park",
+    description:
+      "Plitvice Lakes National Park is a 295-sq.-km forest reserve in central Croatia. It's known for a chain of 16 terraced lakes, joined by waterfalls, that extend into a limestone canyon.",
+    location: "Plitvička Jezera, Croatia",
+  },
 ];
