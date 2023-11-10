b9c712a8fe36b2509779a6c6b2ea7d6674422d4f
Jookz <j.chatterton456@gmail.com>
2023-11-10 10:39:24 +0000

    altered event pictures

--- db/data/dev-data/events.js
+++ db/data/dev-data/events.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     event_id: 1,
-    creator_id: '1',
+    creator_id: "1",
     date: 1697043600000,
     short_description: "Trip to the park",
     description:
@@ -10,11 +10,11 @@ module.exports = [
     latitude: 2.185,
     longitude: 41.416,
     event_picture:
-      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi0.wp.com%2Fwww10.aeccafe.com%2Fblogs%2Farch-showcase%2Ffiles%2F2011%2F07%2F3528.jpg&tbnid=wQaVgQqq7lWxeM&vet=12ahUKEwjtg-61m7eCAxUIWKQEHbqtD18QMygIegQIARBb..i&imgrefurl=https%3A%2F%2Fwww10.aeccafe.com%2Fblogs%2Farch-showcase%2F2011%2F07%2F14%2Fsagrera-linear-park-in-barcelona-spain-by-west-8-urban-design-landscape-architecture%2F&docid=THwZNtU5W2_aHM&w=600&h=281&q=Sagrera%20png&ved=2ahUKEwjtg-61m7eCAxUIWKQEHbqtD18QMygIegQIARBb",
+      "https://media.timeout.com/images/100669361/750/562/image.jpg",
   },
   {
     event_id: 2,
-    creator_id: '2',
+    creator_id: "2",
     date: 1697444000000,
     short_description: "Trip to the beach",
     description:
@@ -23,12 +23,12 @@ module.exports = [
     latitude: 2.189,
     longitude: 41.381,
     event_picture:
-      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.beforetravel.co.uk%2Fwp-content%2Fuploads%2F2023%2F01%2FBeaches-Of-Spain-Barcelona-.png&tbnid=itZCfGIaehTRpM&vet=12ahUKEwiX2MzWmbeCAxXzUqQEHeTQDGYQMygSegUIARCRAQ..i&imgrefurl=https%3A%2F%2Fwww.beforetravel.co.uk%2Fbeaches-of-spain-barcelona%2F&docid=V-8Cw06AlVuP9M&w=480&h=270&q=barceloneta%20png&ved=2ahUKEwiX2MzWmbeCAxXzUqQEHeTQDGYQMygSegUIARCRAQ",
+      "https://media.cnn.com/api/v1/images/stellar/prod/180528121348-04-barcelona-beach-guide.jpg?q=w_3800,h_2137,x_0,y_0,c_fill",
   },
 
   {
     event_id: 3,
-    creator_id: '3',
+    creator_id: "3",
     date: 1697445600000,
     short_description: "Trip to the vineyard",
     description:
@@ -37,11 +37,11 @@ module.exports = [
     latitude: 7.686,
     longitude: 45.07,
     event_picture:
-      "https://www.google.com/imgres?imgurl=x-raw-image%3A%2F%2F%2Fadf539bf6a7e0dad870077e839aa3474757288e700eab68e7f4a214b6178a710&tbnid=rQ1-ar859a2Y8M&vet=12ahUKEwjBwpSrmreCAxVgsycCHZJVBMoQMygQegQIARBT..i&imgrefurl=http%3A%2F%2Fwww.directwineshipments.com%2Fwp-content%2Fuploads%2F2017%2F08%2FDWS-Retail-List-0817.pdf&docid=Pc1jXXiQQ-Jk4M&w=483&h=241&q=torina%20vineyard%20png&ved=2ahUKEwjBwpSrmreCAxVgsycCHZJVBMoQMygQegQIARBT",
+      "https://www.the-wyndham.co.uk/wp-content/uploads/2021/03/shutterstock_1815556148-scaled.jpg",
   },
   {
     event_id: 4,
-    creator_id: '4',
+    creator_id: "4",
     date: 1899458400000,
     short_description: "Trip to the sea",
     description:
@@ -50,11 +50,11 @@ module.exports = [
     latitude: 12.565,
     longitude: 44.067,
     event_picture:
-      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.pixers.pics%2Fpho_wat(s3%3A700%2FFO%2F64%2F10%2F36%2F21%2F700_FO64103621_651b8c7e347aa11a0a1f04a658f07499.jpg%2C700%2C450%2Ccms%3A2018%2F10%2F5bd1b6b8d04b8_220x50-watermark.png%2Cover%2C480%2C400%2Cjpg)%2Fstickers-umbrellas-and-sunbeds-at-rimini-beach-italy-vintage-retro.jpg.jpg&tbnid=z-AYlYWAiX_aiM&vet=12ahUKEwjGqsTMm7eCAxUypycCHSoBA28QMygJegQIARBm..i&imgrefurl=https%3A%2F%2Fpixers.uk%2Fstickers%2Fumbrellas-and-sunbeds-at-rimini-beach-italy-vintage-retro-64103621&docid=EWUecFR9HJhG8M&w=700&h=450&itg=1&q=Rimini%20beach%20png&ved=2ahUKEwjGqsTMm7eCAxUypycCHSoBA28QMygJegQIARBm",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8vJiv56hnfajw5La4Z1Ple4D37HsDcShpSA&usqp=CAU",
   },
   {
     event_id: 5,
-    creator_id: '5',
+    creator_id: "5",
     date: 1704240000000,
     short_description: "Trip to the aquarium",
     description:
@@ -63,24 +63,24 @@ module.exports = [
     latitude: 9.993,
     longitude: 53.551,
     event_picture:
-      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.pikbest.com%2F09%2F14%2F13%2F06kpIkbEsTYSv.jpg!w700wp&tbnid=2XUreLHQGPNaLM&vet=12ahUKEwi2-banm7eCAxVrRKQEHSaQCmUQMygVegUIARCeAQ..i&imgrefurl=https%3A%2F%2Fpikbest.com%2Fbackgrounds%2Fdeep-sea-blue-aquarium-filled-landscape-background-8k_9141306.html&docid=l_u83y8FufS81M&w=700&h=392&q=aquarium%20png&ved=2ahUKEwi2-banm7eCAxVrRKQEHSaQCmUQMygVegUIARCeAQ",
+      "https://media.cnn.com/api/v1/images/stellar/prod/180619151734-aquarium-design-4.jpg?q=w_2598,h_1732,x_0,y_0,c_fill/h_618",
   },
   {
     event_id: 6,
-    creator_id: '6',
+    creator_id: "6",
     date: 1856720000000,
-    short_description: "Trip to the mountains on a bike",
+    short_description: "Cycling trip to the mountains",
     description:
       "Calling all fellow bike lovers - lets meet and have an amazing time biking in the mountains in Montserrat",
     location: "Montserrat, Spain",
     latitude: 1.834,
     longitude: 41.595,
     event_picture:
-      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.montserratvisita.com%2Fwp-content%2Fuploads%2F2018%2F08%2FMontserrat-1.jpg&imgrefurl=https%3A%2F%2Fwww.montserratvisita.com%2Fen%2Fmontserrat%2F&docid=7rN0ZpJQH9h5IM&w=1200&h=800&q=montserrat%20png&ved=2ahUKEwiG3YrJnbeCAxWZQKQEHXQaCqAQMygAegQIARAx",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbysc6bOqj9VehGEHZUyT2KI_fr9ZRkit4RQ&usqp=CAU",
   },
   {
     event_id: 7,
-    creator_id: '7',
+    creator_id: "7",
     date: 1967296000000,
     short_description: "Trip to the lake",
     description:
@@ -89,11 +89,11 @@ module.exports = [
     latitude: 6.066,
     longitude: 60.391,
     event_picture:
-      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.fjordtours.com%2Fwp-content%2Fuploads%2F2019%2F04%2FGeirangerfjord-1.jpg&imgrefurl=https%3A%2F%2Fwww.fjordtours.com%2Fthings-to-do-in-norway%2Ffjords%2Fgeirangerfjord%2F&docid=Z2V2qY2oZ5i5GM&w=1920&h=1080&q=geirangerfjord%20png&ved=2ahUKEwj8sJ7QnbeCAxXZQKQEHX4aCqAQMygAegQIARAx",
+      "https://www.celebritycruises.com/blog/content/uploads/2022/06/lakes-in-norway-lovatnet-olden-hero.jpg",
   },
   {
     event_id: 8,
-    creator_id: '8',
+    creator_id: "8",
     date: 1967296000000,
     short_description: "Trip to the lake",
     description:
@@ -102,11 +102,11 @@ module.exports = [
     latitude: 6.066,
     longitude: 60.391,
     event_picture:
-      "https://smapse.com/image/full/name/top-5-best-places-to-visit-in-northern-norway-amazing-land-of-vikings3-smapse-banner.png/",
+      "https://www.campervannorway.com/assets/img/blog/425/shutterstock_533637949-compressed.jpg",
   },
   {
     event_id: 9,
-    creator_id: '9',
+    creator_id: "9",
     date: 1391212800000,
     short_description: "Trip to the lake",
     description:
@@ -115,11 +115,11 @@ module.exports = [
     latitude: 6.066,
     longitude: 60.391,
     event_picture:
-      "https://smapse.com/image/full/name/top-5-best-places-to-visit-in-northern-norway-amazing-land-of-vikings2-smapse-banner.png/",
+      "https://bstatic.com/xdata/images/xphoto/1182x887/69215973.jpg?k=e7761971e4e0f40fcf7064715c4d93b91472cf73f622b4bca416a51946c111cb&o=?size=S",
   },
   {
     event_id: 10,
-    creator_id: '10',
+    creator_id: "10",
     date: 1435747200000,
     short_description: "Trip to the canal",
     description:
@@ -128,182 +128,6 @@ module.exports = [
     latitude: 4.899,
     longitude: 52.371,
     event_picture:
-      "https://www.pickfords.co.uk/images/default-source/default-album/untitled-design-(20)-(1).png?sfvrsn=25d29003_0",
-  },
-  {
-    event_id: 2,
-    creator_id: 1,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fmuseupicassobcn.cat%2Fen%2Fplan-your-visit%2Fbuy-tickets-and-opening-hours&psig=AOvVaw3wlXzKyQRtYTuVHxVWYoCi&ust=1699628857723000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOi8oYWZt4IDFQAAAAAdAAAAABAE",
-    date: 1694451600000,
-    short_description: "Trip to the Picasso Museum",
-    description:
-      "Calling all fellow art enthusiasts! Picasso Museum was on my to-see-list for a very long time now and the entry fee is only £15pp.",
-    location: "C/ de Montcada, 15-23, 08003 Barcelona, Spain",
-  },
-  {
-    event_id: 3,
-    creator_id: 2,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.britannica.com%2Ftopic%2FPalace-of-Diocletian&psig=AOvVaw1rXA8uJ9CEgMW1JgKT8r0F&ust=1699628906351000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIjCuJyZt4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the Diocletian's Palace",
-    description:
-      "Diocletian's Palace is a huge, fortress-like palace served as a residence for a Roman Emperor & the military garrison.",
-    location: "C/ de Montcada, 15-23, 08003 Barcelona, Spain",
-  },
-  {
-    event_id: 4,
-    creator_id: 2,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.npr.org%2Fsections%2Fparallels%2F2013%2F09%2F30%2F227738868%2Fa-history-of-love-gone-wrong-all-in-one-croatian-museum&psig=AOvVaw3Rdcx9nsRKisNwuIzgOrfF&ust=1699628953483000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKiHkbOZt4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the museum of broken relationships",
-    description:
-      "The Museum of Broken Relationships is a museum in Zagreb, Croatia, dedicated to failed love relationships.",
-    location: "Ćirilometodska ul. 2, 10000, Zagreb, Croatia",
-  },
-  {
-    event_id: 5,
-    creator_id: 3,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.lonelyplanet.com%2Fcroatia%2Fdalmatia%2Fsplit%2Fattractions%2Fklis-fortress%2Fa%2Fpoi-sig%2F1544570%2F358789&psig=AOvVaw0I0AZFdFXfTNaQHem9pPlA&ust=1699629000697000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDLnsmZt4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the fortress of Klis",
-    description:
-      "Klis Fortress is a medieval fortress situated above a village bearing the same name, near the city of Split, in central Dalmatia, Croatia.",
-    location: "Kliška tvrđava, 21231, Klis, Croatia",
-  },
-  {
-    event_id: 6,
-    creator_id: 3,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.sail-croatia.com%2Fset-sail%2Fthe-ultimate-guide-to-krka-national-park&psig=AOvVaw0q6MBaI8Aokw4KpDCdVLUW&ust=1699629049282000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJiauuCZt4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the Krka National Park",
-    description:
-      "Krka National Park is one of the Croatian national parks, named after the river Krka that it encloses.",
-    location: "Lozovac, Croatia",
-  },
-  {
-    event_id: 7,
-    creator_id: 4,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.akvillas.com%2Fdestinations%2Fcroatia%2Fexplore-the-stunning-plitvice-lakes&psig=AOvVaw1SZPC6hiCKYaTkyt36_7pH&ust=1699629100042000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMjHsfiZt4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the Plitvice Lakes National Park",
-    description:
-      "Plitvice Lakes National Park is a 295-sq.-km forest reserve in central Croatia. It's known for a chain of 16 terraced lakes, joined by waterfalls, that extend into a limestone canyon.",
-    location: "Plitvička Jezera, Croatia",
-  },
-  {
-    event_id: 8,
-    creator_id: 4,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.timeout.com%2Fcroatia%2Fthings-to-do%2Fthings-to-do-in-dubrovniks-old-town&psig=AOvVaw0KPLERx5KJbdZIyaq4sZx0&ust=1699629138751000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjhy4qat4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the Dubrovnik Old Town",
-    description:
-      "Dubrovnik is a city in southern Croatia fronting the Adriatic Sea. It's known for its distinctive Old Town, encircled with massive stone walls completed in the 16th century.",
-    location: "Dubrovnik, Croatia",
-  },
-  {
-    event_id: 9,
-    creator_id: 5,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.visitzagreb.hr%2Fzagreb%2Fzagreb-cathedral%2F&psig=AOvVaw0JDHYrJzTw-lertNA-gAeL&ust=1699629272294000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCn7Mqat4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the Zagreb Cathedral",
-    description:
-      "Zagreb Cathedral on Kaptol is a Roman Catholic institution and not only the tallest building in Croatia but also the most monumental sacral building in Gothic style southeast of the Alps.",
-    location: "Kaptol 31, 10000, Zagreb, Croatia",
-  },
-  {
-    event_id: 10,
-    creator_id: 5,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.visitzagreb.hr%2Fzagreb%2Fmirogoj-cemetery%2F&psig=AOvVaw1GpN7oyjQdZShZNoAUmF2h&ust=1699629309059000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMClp9yat4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the Mirogoj Cemetery",
-    description:
-      "The Mirogoj Cemetery is a cemetery park that is considered to be among the more noteworthy landmarks in the City of Zagreb.",
-    location: "Aleja Hermanna Bollea 27, 10000, Zagreb, Croatia",
-  },
-  {
-    event_id: 11,
-    creator_id: 6,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.npr.org%2Fsections%2Fparallels%2F2013%2F09%2F30%2F227738868%2Fa-history-of-love-gone-wrong-all-in-one-croatian-museum&psig=AOvVaw3Rdcx9nsRKisNwuIzgOrfF&ust=1699628953483000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKiHkbOZt4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the Museum of Broken Relationships",
-    description:
-      "The Museum of Broken Relationships is a museum in Zagreb, Croatia, dedicated to failed love relationships.",
-    location: "Ćirilometodska ul. 2, 10000, Zagreb, Croatia",
-  },
-  {
-    event_id: 12,
-    creator_id: 6,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.visitzagreb.hr%2Fzagreb%2Fzagreb-cathedral%2F&psig=AOvVaw0JDHYrJzTw-lertNA-gAeL&ust=1699629272294000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCn7Mqat4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the Zagreb Cathedral",
-    description:
-      "Zagreb Cathedral on Kaptol is a Roman Catholic institution and not only the tallest building in Croatia but also the most monumental sacral building in Gothic style southeast of the Alps.",
-    location: "Kaptol 31, 10000, Zagreb, Croatia",
-  },
-  {
-    event_id: 13,
-    creator_id: 7,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.britannica.com%2Ftopic%2FPalace-of-Diocletian&psig=AOvVaw1rXA8uJ9CEgMW1JgKT8r0F&ust=1699628906351000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIjCuJyZt4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the Diocletian's Palace",
-    description:
-      "Diocletian's Palace is a huge, fortress-like palace served as a residence for a Roman Emperor & the military garrison.",
-    location: "C/ de Montcada, 15-23, 08003 Barcelona, Spain",
-  },
-  {
-    event_id: 14,
-    creator_id: 7,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.npr.org%2Fsections%2Fparallels%2F2013%2F09%2F30%2F227738868%2Fa-history-of-love-gone-wrong-all-in-one-croatian-museum&psig=AOvVaw3Rdcx9nsRKisNwuIzgOrfF&ust=1699628953483000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKiHkbOZt4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the museum of broken relationships",
-    description:
-      "The Museum of Broken Relationships is a museum in Zagreb, Croatia, dedicated to failed love relationships.",
-    location: "Ćirilometodska ul. 2, 10000, Zagreb, Croatia",
-  },
-  {
-    event_id: 15,
-    creator_id: 8,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.lonelyplanet.com%2Fcroatia%2Fdalmatia%2Fsplit%2Fattractions%2Fklis-fortress%2Fa%2Fpoi-sig%2F1544570%2F358789&psig=AOvVaw0I0AZFdFXfTNaQHem9pPlA&ust=1699629000697000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDLnsmZt4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the fortress of Klis",
-    description:
-      "Klis Fortress is a medieval fortress situated above a village bearing the same name, near the city of Split, in central Dalmatia, Croatia.",
-    location: "Kliška tvrđava, 21231, Klis, Croatia",
-  },
-  {
-    event_id: 16,
-    creator_id: 8,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.sail-croatia.com%2Fset-sail%2Fthe-ultimate-guide-to-krka-national-park&psig=AOvVaw0q6MBaI8Aokw4KpDCdVLUW&ust=1699629049282000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJiauuCZt4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the Krka National Park",
-    description:
-      "Krka National Park is one of the Croatian national parks, named after the river Krka that it encloses.",
-    location: "Lozovac, Croatia",
-  },
-  {
-    event_id: 17,
-    creator_id: 9,
-    event_img:
-      "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.akvillas.com%2Fdestinations%2Fcroatia%2Fexplore-the-stunning-plitvice-lakes&psig=AOvVaw1SZPC6hiCKYaTkyt36_7pH&ust=1699629100042000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMjHsfiZt4IDFQAAAAAdAAAAABAE",
-    date: 1707670800000,
-    short_description: "Trip to the Plitvice Lakes National Park",
-    description:
-      "Plitvice Lakes National Park is a 295-sq.-km forest reserve in central Croatia. It's known for a chain of 16 terraced lakes, joined by waterfalls, that extend into a limestone canyon.",
-    location: "Plitvička Jezera, Croatia",
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/28/74/c9/cf/caption.jpg?w=700&h=-1&s=1",
   },
 ];
