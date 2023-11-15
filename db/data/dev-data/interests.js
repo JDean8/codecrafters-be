c019c91e2e26b8147683d134b98af7440dfab1a9
karolinakonior <karolina.koniorr@gmail.com>
2023-11-15 16:06:47 +0000

    Fixed interests

--- db/data/dev-data/interests.js
+++ db/data/dev-data/interests.js
@@ -33,182 +33,170 @@ module.exports = [
   },
   {
     interest_id: 9,
-    interest: "swimming",
-  },
-  {
-    interest_id: 10,
-    interest: "swimming",
-  },
-  {
-    interest_id: 11,
     interest: "videography",
   },
   {
-    interest_id: 12,
+    interest_id: 10,
     interest: "shopping",
   },
   {
-    interest_id: 13,
+    interest_id: 11,
     interest: "food tasting",
   },
   {
-    interest_id: 14,
+    interest_id: 12,
     interest: "cooking",
   },
   {
-    interest_id: 15,
+    interest_id: 13,
     interest: "hiking",
   },
   {
-    interest_id: 16,
+    interest_id: 14,
     interest: "fishing",
   },
   {
-    interest_id: 17,
+    interest_id: 15,
     interest: "animal watching",
   },
   {
-    interest_id: 18,
+    interest_id: 16,
     interest: "stargazing",
   },
   {
-    interest_id: 19,
+    interest_id: 17,
     interest: "bird watching",
   },
   {
-    interest_id: 20,
+    interest_id: 18,
     interest: "camping",
   },
   {
-    interest_id: 21,
+    interest_id: 19,
     interest: "gardening",
   },
   {
-    interest_id: 22,
+    interest_id: 20,
     interest: "reading",
   },
   {
-    interest_id: 23,
+    interest_id: 21,
     interest: "writing",
   },
   {
-    interest_id: 24,
-    interest: "dancing",
-  },
-  {
-    interest_id: 25,
+    interest_id: 22,
     interest: "singing",
   },
   {
-    interest_id: 26,
+    interest_id: 23,
     interest: "acting",
   },
   {
-    interest_id: 27,
+    interest_id: 24,
     interest: "painting",
   },
   {
-    interest_id: 28,
+    interest_id: 25,
     interest: "drawing",
   },
   {
-    interest_id: 29,
+    interest_id: 26,
     interest: "sketching",
   },
   {
-    interest_id: 30,
+    interest_id: 27,
     interest: "sculpting",
   },
   {
-    interest_id: 31,
+    interest_id: 28,
     interest: "pottery",
   },
   {
-    interest_id: 32,
+    interest_id: 29,
     interest: "woodworking",
   },
   {
-    interest_id: 33,
+    interest_id: 30,
     interest: "knitting",
   },
   {
-    interest_id: 34,
+    interest_id: 31,
     interest: "sewing",
   },
   {
-    interest_id: 35,
+    interest_id: 32,
     interest: "quilting",
   },
   {
-    interest_id: 36,
+    interest_id: 33,
     interest: "crocheting",
   },
   {
-    interest_id: 37,
+    interest_id: 34,
     interest: "embroidery",
   },
   {
-    interest_id: 38,
+    interest_id: 35,
     interest: "cross-stitch",
   },
   {
-    interest_id: 39,
+    interest_id: 36,
     interest: "weaving",
   },
   {
-    interest_id: 40,
+    interest_id: 37,
     interest: "spinning",
   },
   {
-    interest_id: 41,
+    interest_id: 38,
     interest: "quilting",
   },
   {
-    interest_id: 42,
+    interest_id: 39,
     interest: "origami",
   },
   {
-    interest_id: 43,
+    interest_id: 40,
     interest: "calligraphy",
   },
   {
-    interest_id: 44,
+    interest_id: 41,
     interest: "jewelry making",
   },
   {
-    interest_id: 45,
+    interest_id: 42,
     interest: "beading",
   },
   {
-    interest_id: 46,
+    interest_id: 43,
     interest: "scrapbooking",
   },
   {
-    interest_id: 47,
+    interest_id: 44,
     interest: "card making",
   },
   {
-    interest_id: 48,
+    interest_id: 45,
     interest: "metalworking",
   },
   {
-    interest_id: 49,
+    interest_id: 46,
     interest: "glassblowing",
   },
   {
-    interest_id: 50,
+    interest_id: 47,
     interest: "pottery",
   },
   {
-    interest_id: 51,
+    interest_id: 48,
     interest: "leatherworking",
   },
   {
-    interest_id: 52,
+    interest_id: 49,
     interest: "soapmaking",
   },
   {
-    interest_id: 53,
+    interest_id: 50,
     interest: "candlemaking",
   },
 ];
